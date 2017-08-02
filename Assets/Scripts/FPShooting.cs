2ef1279bafc78de90a0176dd9394d63266d9ba49
seattlefella <thughes@valnetventures.com>
2017-08-01 19:28:45 -0700

    Simple-FPS Added: Fire Rate; refactored target damage moving it to the target; Shooting with both RayCasting and projectiles now work.

--- Assets/Scripts/FPShooting.cs
+++ Assets/Scripts/FPShooting.cs
@@ -4,6 +4,16 @@ namespace Assets.Scripts
 {
 	public class FPShooting : MonoBehaviour
 	{
+		// General variables do define behavior
+		[SerializeField]
+		private bool isSingleFire;
+		[SerializeField]
+		private float fireRate;
+
+		private Target tempComponent;
+
+		private delegate void shootMethod();
+		private shootMethod shoot;
 
 		// Variables needed by projectile munitions
 		[Header("Parameters needed to shoot a GameObject")]
@@ -14,14 +24,15 @@ namespace Assets.Scripts
 		[SerializeField]
 		private bool isProjectile;
 		private GameObject launchedBullet;
+		[SerializeField]
+		private float lifeTime;
 
 		// Variables needed by ray cast munitions
 		[SerializeField]
 		private bool isRayCast;
 		[SerializeField]
-		private float fireRate;
-		[SerializeField]
 		private float range;
+
 		private RaycastHit hitInfo;
 		private Ray ray ;
 		private Vector3 hitPoint;
@@ -36,27 +47,39 @@ namespace Assets.Scripts
 			playerCamera = GameObject.FindGameObjectWithTag("FPCamera");
 			ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
 
+			if (isProjectile && !isRayCast)
+			{
+				 shoot = ShootProjectile;
+			}
+
+			if (isRayCast)
+			{
+				shoot = ShootRayCast;
+			}
+
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
 
-			if (Input.GetButtonDown("Fire1"))
+			if (Input.GetButtonDown("Fire1") && isSingleFire)
 			{
-
-				if (isProjectile && !isRayCast)
-				{
-					ShootProjectile();
-				}
-
-				if (isProjectile && !isRayCast)
-				{
-					ShootRayCast();
-				}
-
+				shoot();				
 			}
-		
+
+			else
+			{
+				if (Input.GetButtonDown("Fire1"))
+				{
+					InvokeRepeating(shoot.Method.Name, 0f, 1f / fireRate);
+				}
+				else if (Input.GetButtonUp("Fire1"))
+				{
+					CancelInvoke(shoot.Method.Name);
+				}
+			}
+
 		}
 
 		// this method will actually use the physics system to move a game object through space
@@ -67,32 +90,35 @@ namespace Assets.Scripts
 		{
 
 			// Todo: Add logic to limit the rate of fire.
+			// Todo: Convert the different types of shooters into delegates that are selected at startup
+			// Todo: Move I have been hit logic to the target - make it his responsibility
 			// Todo: Add logic to limit ammunition.
 			// Todo: Add logic to reload.
 
+
+
 			launchedBullet = (GameObject)Instantiate(bullet, playerCamera.transform.position + playerCamera.transform.forward * 1, playerCamera.transform.rotation);
 			launchedBullet.GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward * initialVelocity, ForceMode.Impulse);
+			// The munition missed everything so simply destroy it at the end of its life time
+			Destroy(launchedBullet, lifeTime);
+			// The target senses via OnCollisionEnter And takes action
 		}
 
 		public void ShootRayCast()
 		{
-
-			// Todo: Add logic to limit the rate of fire.
-			// Todo: Add logic to limit ammunition.
-			// Todo: Add logic to reload.
-//			Ray ray = new Ray(playerCamera.transform.position,playerCamera.transform.forward);
 			ray.origin = playerCamera.transform.position;
 			ray.direction = playerCamera.transform.forward;
 
-
 			if(Physics.Raycast(ray, out hitInfo, range))
 			{
-				hitPoint = hitInfo.point;
-				// Send a call to the target that was hit so it shows that it was hit.
+				// Send a call to the target that was hit so it can take appropriate action
+				tempComponent = hitInfo.transform.gameObject.GetComponent<Target>();
+				if (tempComponent != null)
+				{
+					// We will have to send data on the munition that hit it so the correct effect can be implemented
+					tempComponent.OnShot(hitInfo, isRayCast);
+				}
 			}
-
-
-
 		}
 	}
 }
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Target : MonoBehaviour {
+
+        [SerializeField]
+        private GameObject tempDamage;
+
+        [SerializeField]
+        private float lifeTime;
+
+        [SerializeField]
+        private string triggerTag;
+
+        [SerializeField]
+        private GameObject explosion;
+
+        [SerializeField]
+        private Renderer renderer;
+
+        // This method responds to the Target being hit by a rayShooter
+        public void OnShot(RaycastHit hitInfo, bool isRayCast)
+        {
+            if (isRayCast)
+            {
+                //  Instantiate(tempDamage, hitInfo.point, Quaternion.identity);      
+                Explosion();
+            }
+
+        }
+
+
+        // This method responds to the target being hit by a munition
+        void OnCollisionEnter(Collision collision)
+
+        {
+            if (collision.gameObject.tag == triggerTag)
+            {
+                // We must destroy the gameObject of the munition that hit us.
+                Destroy(collision.gameObject);
+                Explosion();
+            }
+
+        }
+
+        void Explosion()
+        {
+                renderer.enabled = false;
+                explosion.SetActive(true);
+        }
+
+    }
+}
