3eeeadbc0305e93670d5f625f5ab12314000a516
seattlefella <thughes@valnetventures.com>
2017-08-05 19:40:11 -0700

    Simple-FPS - minor refactoring, thinking about how best to fill/empty Magazines and Clips of bullets.  Really this is just an end of day save as I power down.

--- Assets/Scripts/Magazine.cs
+++ Assets/Scripts/Magazine.cs
@@ -1,11 +1,36 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class Magazine : MonoBehaviour {
 
-        public 
+  
+
+    }
+
+    public interface IMagizine
+    {
+        Clip Reload(Clip clip);
+        Munition GetMunition();
+
+        Clip CreateClip();
+
+        int DestroyClip(Clip clip);
+
+    }
+
+    // a data structure that holds a collection of munitions  
+    public class Clip
+    {
+        public int Size;
+        public int CurrentCount;
+        public List<Munition> ActiveClip = new List<Munition>();
+        public bool IsEmpty;
+        public bool IsReloading;
+
 
     }
 }
